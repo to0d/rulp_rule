e46167f96e6936cb438f04896ae6b95c71d85739
todd <to0d@outlook.com>
2021-09-12 14:12:01 +0800

    up

--- src/alpha/rulp/ximpl/rbs/Constant.java
+++ src/alpha/rulp/ximpl/rbs/Constant.java
@@ -5,21 +5,9 @@ public interface Constant {
 
 	String RBS_NS = "rbs";
 
-	String STR_SCHEMA_NAME = "_RS_SCHEMA";
-
-	String STR_TABLE = "_RS_TABLE";
-
 	String F_PARSE_COLUMN_DEF_LIST = "parse-column-def-list";
 
 	String F_PARSE_VALUES_LIST = "parse-values-list";
-
-	String F_HAS_SQL_TABLE_NAME = "has-sql-table-names";
-
-	String F_ADD_SQL_TABLE = "add-sql-table";
-
-	String F_ADD_SQL_COLUMN = "add-sql-column";
-
-	String F_RBS_CREATE_TABLE = "_rbs_create_table";
 
 	int SQLCODE_SUCC = 0;
 }
--- src/alpha/rulp/ximpl/rbs/RBSUtil.java
+++ src/alpha/rulp/ximpl/rbs/RBSUtil.java
@@ -1,9 +1,7 @@
 package alpha.rulp.ximpl.rbs;
 
-import static alpha.rulp.ximpl.rbs.Constant.F_HAS_SQL_TABLE_NAME;
 import static alpha.rulp.ximpl.rbs.Constant.F_PARSE_COLUMN_DEF_LIST;
 import static alpha.rulp.ximpl.rbs.Constant.F_PARSE_VALUES_LIST;
-import static alpha.rulp.ximpl.rbs.Constant.STR_SCHEMA_NAME;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,49 +11,15 @@ import java.util.Set;
 
 import alpha.rulp.lang.IRArray;
 import alpha.rulp.lang.IRFrame;
-import alpha.rulp.lang.IRFrameEntry;
 import alpha.rulp.lang.IRList;
 import alpha.rulp.lang.IRObject;
 import alpha.rulp.lang.RException;
 import alpha.rulp.lang.RType;
-import alpha.rulp.rule.IRModel;
 import alpha.rulp.runtime.IRInterpreter;
 import alpha.rulp.utils.LoadUtil;
-import alpha.rulp.utils.RuleUtil;
 import alpha.rulp.utils.RulpUtil;
 
 public class RBSUtil {
-
-	public static RSQLSchema asSchema(IRObject obj) throws RException {
-
-		if (!(obj instanceof RSQLSchema)) {
-			throw new RException("Can't convert to schema: " + obj);
-		}
-
-		return (RSQLSchema) obj;
-	}
-
-	public static RSQLSchema getSchema(IRModel model) throws RException {
-
-		IRFrame modelFrame = model.getModelFrame();
-
-		IRFrameEntry schemaFrameEntry = modelFrame.getEntry(STR_SCHEMA_NAME);
-		if (schemaFrameEntry != null && schemaFrameEntry.getFrame() != modelFrame) {
-			return RBSUtil.asSchema(schemaFrameEntry.getObject());
-		}
-
-		RSQLSchema schema = new RSQLSchema(model);
-		modelFrame.setEntry(STR_SCHEMA_NAME, schema);
-		return schema;
-	}
-
-	public static boolean hasTableNames(IRModel model, String tableName) throws RException {
-
-		List<IRObject> rst = RuleUtil.compute(model,
-				String.format("(%s %s \"%s\")", F_HAS_SQL_TABLE_NAME, model.getModelName(), tableName));
-
-		return rst.size() == 1 && RulpUtil.asBoolean(rst.get(0)).asBoolean();
-	}
 
 	public static void init(IRInterpreter interpreter, IRFrame frame) throws RException, IOException {
 
