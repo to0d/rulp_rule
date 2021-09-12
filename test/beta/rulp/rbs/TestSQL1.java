a158c692c7f28bdda5ab6fc52f0bde2bc8197004
todd <to0d@outlook.com>
2021-09-12 10:32:38 +0800

    up

--- test/beta/rulp/rbs/TestSQL1.java
+++ test/beta/rulp/rbs/TestSQL1.java
@@ -4,21 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import alpha.rulp.utils.RuleTestBase;
 
-class XRFactorCreateTableTest extends RuleTestBase {
+class TestSQL1 extends RuleTestBase {
 
 	@Test
 	void test_1_create_table_1() {
 
 		_setup();
-		_clean_cache("result/rbs/XRFactorCreateTableTest/test_1_create_table_1");
+		_clean_cache("result/rbs/TestSQL1/test_1_create_table_1");
 		_test("(use namespace rbs)");
 		_test("(new model m)");
 		_test("(set schema m)");
 		_test("(create table name1 {c1 int, c2 bool, c3 float})", "0");
-		_statsInfo("m", "result/rbs/XRFactorCreateTableTest/test_1_create_table_1.txt");
-		_test("(set-model-cache-path m \"result/rbs/XRFactorCreateTableTest/test_1_create_table_1\")");
+		_statsInfo("m", "result/rbs/TestSQL1/test_1_create_table_1.txt");
+		_test("(set-model-cache-path m \"result/rbs/TestSQL1/test_1_create_table_1\")");
 		_test("(save-model m)");
 	}
 
-	 
 }
