2ca87bb19770a4ccb1ff65a23d0dfc1d6e0a55ab
todd <to0d@outlook.com>
2021-12-11 16:20:26 +0800

    up

--- test/beta/rulp/factor/XRFactorRemoveStmtTest.java
+++ test/beta/rulp/factor/XRFactorRemoveStmtTest.java
@@ -10,11 +10,7 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_3_varargs_1() {
 
 		_setup();
-
-		_test("(new model m)");
-		_test("(add-stmt m '(a b c1))");
-		_test("(add-stmt m n1:'(a b c1))");
-		_test("(remove-stmt m '(?...))", "'('(a b c1))");
+		_run_script();
 
 		_mStatus(1, "m");
 		_mCount(1, "m");
@@ -26,12 +22,7 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_3_varargs_2() {
 
 		_setup();
-
-		_test("(new model m)");
-		_test("(add-stmt m '(a b c1))");
-		_test("(add-stmt m n1:'(a b c1))");
-		_test("(remove-stmt m '(?x ?...))", "'('(a b c1))");
-
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
@@ -42,12 +33,7 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_3_varargs_3() {
 
 		_setup();
-
-		_test("(new model m)");
-		_test("(add-stmt m '(a b c1))");
-		_test("(add-stmt m n1:'(a b c1))");
-		_test("(remove-stmt m '(a ?...))", "'('(a b c1))");
-
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
@@ -58,12 +44,7 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_3_varargs_4() {
 
 		_setup();
-
-		_test("(new model m)");
-		_test("(add-stmt m '(a b c1))");
-		_test("(add-stmt m n1:'(a b c1))");
-		_test("(remove-stmt m n2:'(?...))", "'()");
-
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
@@ -74,13 +55,7 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_3_varargs_5() {
 
 		_setup();
-
-		_test("(new model m)");
-		_test("(add-stmt m n1:'(a b c1))");
-		_test("(add-stmt m n1:'(a b c2))");
-		_test("(add-stmt m n1:'(a2 b c2))");
-		_test("(remove-stmt m n1:'(?...))", "'(n1:'(a b c1) n1:'(a b c2) n1:'(a2 b c2))");
-
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
@@ -91,13 +66,7 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_3_varargs_6() {
 
 		_setup();
-
-		_test("(new model m)");
-		_test("(add-stmt m n1:'(a b c1))");
-		_test("(add-stmt m n1:'(a b c2))");
-		_test("(add-stmt m n1:'(a2 b c2))");
-		_test("(remove-stmt m n1:'(? ?...))", "'(n1:'(a b c1) n1:'(a b c2) n1:'(a2 b c2))");
-
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
@@ -151,29 +120,11 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_conflict_1() {
 
 		_setup();
-		// XRModel.TRACE_RETE = true;
-		_test("(new model m)", "m");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?x) do (remove-stmt m '(?x p1 ?y)))", "RU000");
-		_test("(add-stmt m '(a p1 b))", "true");
-		_test("(add-stmt m '(b p1 a))", "true");
-		_test("(list-stmt m from '(?x ?y ?z))", "'('(a p1 b) '(b p1 a))");
-		_test("(state-of m)", "runnable");
+		_run_script();
+		_statsInfo("m");
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
-
-		_test("(start m)", "4");
-		_test("(state-of m)", "completed");
-		_test("(list-stmt m from '(?x ?y ?z))", "'('(b p1 a))");
-		_mStatus(2, "m");
-		_mCount(2, "m");
-		_eCount(2, "m");
-
-		_test("(gc-model m)", "4");
-		_mStatus(3, "m");
-		_mCount(3, "m");
-		_eCount(3, "m");
-
 		_saveTest();
 	}
 
@@ -181,42 +132,11 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_conflict_2() {
 
 		_setup();
-
-//		XRReteEntryTable.TRACE = true;
-//		XRModel.TRACE_RETE = true;
-
-		_test("(new model m)", "m");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?z) do (add-stmt m '(?x p1 ?z)))", "RU000");
-		_test("(add-stmt m '(a p1 b))", "true");
-		_test("(add-stmt m '(b p1 c))", "true");
-		_test("(add-stmt m '(c p1 a))", "true");
-		_test("(list-stmt m from '(?x ?y ?z))", "'('(a p1 b) '(b p1 c) '(c p1 a))");
-		_test("(state-of m)", "runnable");
+		_run_script();
+		_statsInfo("m");
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
-
-		_test("(start m)", "12");
-		_test("(state-of m)", "completed");
-		_test("(list-stmt m)",
-				"'('(a p1 b) '(b p1 c) '(c p1 a) '(a p1 c) '(b p1 a) '(c p1 b) '(a p1 a) '(b p1 b) '(c p1 c))");
-		_mStatus(2, "m");
-		_mCount(2, "m");
-		_eCount(2, "m");
-
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?x) (not (equal ?x ?y)) do (remove-stmt m '(?x p1 ?y)))", "RU001");
-		_test("(start m)", "5");
-		_test("(state-of m)", "completed");
-		_test("(list-stmt m)", "'('(a p1 b) '(b p1 c) '(c p1 a) '(a p1 a) '(b p1 b) '(c p1 c))");
-		_mStatus(3, "m");
-		_mCount(3, "m");
-		_eCount(3, "m");
-
-		_test("(gc-model m)", "30");
-		_mStatus(4, "m");
-		_mCount(4, "m");
-		_eCount(4, "m");
-
 		_saveTest();
 	}
 
@@ -224,32 +144,10 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	void test_conflict_3() {
 
 		_setup();
-
-		// XRModel.TRACE_RETE = true;
-		_test("(new model m)", "m");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?x) (not (equal ?x ?y)) do (remove-stmt m '(?x p1 ?y)))", "RU000");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?z) do (add-stmt m '(?x p1 ?z)))", "RU001");
-		_test("(add-stmt m '(a p1 b))", "true");
-		_test("(add-stmt m '(b p1 c))", "true");
-		_test("(add-stmt m '(c p1 a))", "true");
-		_test("(list-stmt m from '(?x ?y ?z))", "'('(a p1 b) '(b p1 c) '(c p1 a))");
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
-
-		_test("(start m)", "13");
-		_test("(state-of m)", "completed");
-		// System.out.println(OptimizeUtil.printStatsInfo(_model("m")));
-		_test("(list-stmt m)", "'('(a p1 b) '(b p1 c) '(a p1 c))");
-		_mStatus(2, "m");
-		_mCount(2, "m");
-		_eCount(2, "m");
-
-		_test("(gc-model m)", "26");
-		_mStatus(3, "m");
-		_mCount(3, "m");
-		_eCount(3, "m");
-
 		_saveTest();
 		_statsInfo("m");
 		_refInfo("m");
@@ -259,94 +157,37 @@ class XRFactorRemoveStmtTest extends RuleTestBase {
 	@Test
 	void test_drop_stmt_rule_1() {
 
-		// XRModel.TRACE_RETE = true;
-
 		_setup();
-		_test("(new model m)", "m");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?x) (not (equal ?x ?y)) do"
-				+ " (remove-stmt '(?x p1 ?y)) (remove-stmt '(?y p1 ?x)))", "RU000");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?z) do (add-stmt m '(?x p1 ?z)))", "RU001");
-		_test("(add-stmt m '(a p1 b))", "true");
-		_test("(add-stmt m '(b p1 c))", "true");
-		_test("(add-stmt m '(c p1 a))", "true");
-		_test("(list-stmt m)", "'('(a p1 b) '(b p1 c) '(c p1 a))");
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
-
-		_test("(start m)", "13");
-		_test("(state-of m)", "completed");
-		_test("(list-stmt m)", "'('(a p1 b) '(b p1 c))");
-		_mStatus(2, "m");
-		_mCount(2, "m");
-		_eCount(2, "m");
-
-		_test("(gc-model m)", "29");
-		_mStatus(3, "m");
-		_mCount(3, "m");
-		_eCount(3, "m");
-
 		_saveTest();
-
+		_statsInfo("m");
 	}
 
 	@Test
 	void test_drop_stmt_rule_2() {
 
 		_setup();
-
-		// XRModel.TRACE_RETE = true;
-
-		_test("(new model m)", "m");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?x) (not (equal ?x ?y)) do (remove-stmt ?0) (remove-stmt ?1))",
-				"RU000");
-		_test("(add-rule m if '(?x p1 ?y) '(?y p1 ?z) do (add-stmt m '(?x p1 ?z)))", "RU001");
-		_test("(add-stmt m '(a p1 b))", "true");
-		_test("(add-stmt m '(b p1 c))", "true");
-		_test("(add-stmt m '(c p1 a))", "true");
-		_test("(list-stmt m)", "'('(a p1 b) '(b p1 c) '(c p1 a))");
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
-
-		_test("(start m)", "13");
-		_test("(state-of m)", "completed");
-		_test("(list-stmt m)", "'('(a p1 b) '(b p1 c))");
-		_mStatus(2, "m");
-		_mCount(2, "m");
-		_eCount(2, "m");
-
-		_test("(gc-model m)", "29");
-		_mStatus(3, "m");
-		_mCount(3, "m");
-		_eCount(3, "m");
-
 		_saveTest();
-
+		_statsInfo("m");
 	}
 
 	@Test
 	void test_incomplete_mode() {
 
 		_setup();
-		_test("(new model m)", "m");
-		_test("(add-stmt m '(a p1 c))", "true");
-		_test("(add-stmt m '(a p1 b))", "true");
-		_test("(add-stmt m '(a p2 c))", "true");
-		_test("(list-stmt m)", "'('(a p1 c) '(a p1 b) '(a p2 c))");
-
-		_test("(remove-stmt m '(a p2 c))", "'('(a p2 c))");
-		_test("(list-stmt m)", "'('(a p1 c) '(a p1 b))");
+		_run_script();
 		_mStatus(1, "m");
 		_mCount(1, "m");
 		_eCount(1, "m");
-
-		_test("(gc-model m)", "1");
-		_mStatus(2, "m");
-		_mCount(2, "m");
-		_eCount(2, "m");
-
 		_saveTest();
+		_statsInfo("m");
 	}
 
 }
