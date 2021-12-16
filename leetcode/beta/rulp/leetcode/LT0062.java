cad4fffe5584dfd389d48780179d3949f552efd0
todd-t4 <lylybj@cn.ibm.com>
2021-12-16 10:13:22 +0800

    up

---
+++ leetcode/beta/rulp/leetcode/LT0062.java
@@ -0,0 +1,16 @@
+package beta.rulp.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import alpha.rulp.utils.RuleTestBase;
+
+public class LT0062 extends RuleTestBase {
+
+	@Test
+	public void test_lt0062_1() {
+
+		_setup();
+		_run_script();
+	}
+
+}
