6a8f95d44045f018a011e06ad508ef89ac94f5d0
todd <to0d@outlook.com>
2021-12-24 10:52:18 +0800

    up

--- leetcode/beta/rulp/leetcode/LT0062.java
+++ leetcode/beta/rulp/leetcode/LT0062.java
@@ -9,6 +9,9 @@ public class LT0062 extends RuleTestBase {
 	@Test
 	public void test_lt0062_1() {
 
+		// 1: how to prove: f(m, n) = f(n, m)
+		// 2: dp solution, use 1*n vector
+
 		_setup();
 		_run_script();
 	}
