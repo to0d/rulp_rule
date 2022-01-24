78be40559fefa378f30df393bcef7350a42812f1
todd-t4 <lylybj@cn.ibm.com>
2022-01-24 21:38:12 +0800

    up

--- test/beta/rulp/factor/XRFactorListRuleTest.java
+++ test/beta/rulp/factor/XRFactorListRuleTest.java
@@ -7,22 +7,23 @@ import alpha.rulp.utils.RuleTestBase;
 class XRFactorListRuleTest extends RuleTestBase {
 
 	@Test
-	void test_list_failed_rule() {
+	void test_list_rule_1_fail_rule() {
 
-		// XRModel.TRACE_RETE = true;
 		_setup();
-		_test("(new model m)");
-		_test("(add-rule \"R1\" m if '(?a p1 ?b) do (if (equal ?a ?b) (throw err-r1)) (-> (?b p1 ?a)))");
-		_test("(add-rule \"R2\" m if '(?a p2 ?b) do (-> (?b p1 ?a)))");
-		_test("(add-stmt m '(a p1 a))");
-		_test("(list-rule m)", "'(R1 R2)");
-		_test("(list-with-state (list-rule m) failed)", "'()");
-		_test("(list-without-state (list-rule m) Failed)", "'(R1 R2)");
-
-		_test("(start m)");
-
-		_test("(list-with-state (list-rule m) failed)", "'(R1)");
-		_test("(list-without-state (list-rule m) failed)", "'(R2)");
+		_run_script();
+		
+//		_test("(new model m)");
+//		_test("(add-rule \"R1\" m if '(?a p1 ?b) do (if (equal ?a ?b) (throw err-r1)) (-> (?b p1 ?a)))");
+//		_test("(add-rule \"R2\" m if '(?a p2 ?b) do (-> (?b p1 ?a)))");
+//		_test("(add-stmt m '(a p1 a))");
+//		_test("(list-rule m)", "'(R1 R2)");
+//		_test("(list-with-state (list-rule m) failed)", "'()");
+//		_test("(list-without-state (list-rule m) Failed)", "'(R1 R2)");
+//
+//		_test("(start m)");
+//
+//		_test("(list-with-state (list-rule m) failed)", "'(R1)");
+//		_test("(list-without-state (list-rule m) failed)", "'(R2)");
 	}
 
 }
