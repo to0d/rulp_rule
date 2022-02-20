1b0d9ca57fe3dc605cc87859bd52c3a68747dbd8
todd <to0d@outlook.com>
2022-02-20 10:15:04 +0800

    up

---
+++ test/beta/rulp/basic/TestRuleBase.java
@@ -0,0 +1,16 @@
+package beta.rulp.basic;
+
+import org.junit.jupiter.api.Test;
+
+import alpha.rulp.utils.RuleTestBase;
+
+class TestRuleBase extends RuleTestBase {
+
+	@Test
+	void test_rete_valid_entry_of_1() {
+
+		_setup();
+		_run_script();
+	}
+
+}
