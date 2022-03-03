55fc69b5aa022e18e674f4a07816b010a2a1a491
todd-t4 <lylybj@cn.ibm.com>
2022-03-03 12:20:43 +0800

    up

---
+++ eton/beta/rulp/eton/TestStock.java
@@ -0,0 +1,16 @@
+package beta.rulp.eton;
+
+import org.junit.jupiter.api.Test;
+
+import alpha.rulp.utils.RuleTestBase;
+
+public class TestStock extends RuleTestBase {
+
+	@Test
+	void test_stock_1_model_cache() {
+
+		_setup();
+		_run_script();
+	}
+
+}
