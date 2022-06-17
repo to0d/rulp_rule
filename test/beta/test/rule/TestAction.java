98057b734067efd980fc2ff1aad98a7df277ff36
todd <to0d@outlook.com>
2022-06-17 16:05:20 +0800

    up

--- test/beta/test/rule/TestAction.java
+++ test/beta/test/rule/TestAction.java
@@ -7,6 +7,14 @@ import alpha.rulp.utils.RuleTestBase;
 public class TestAction extends RuleTestBase {
 
 	@Test
+	void test_action_0_external_var() {
+
+		_setup();
+		_run_script();
+		_statsInfo("m");
+	}
+
+	@Test
 	void test_action_1_index_var_a() {
 
 		_setup();
