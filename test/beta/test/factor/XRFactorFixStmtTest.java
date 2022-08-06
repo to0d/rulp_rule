3170b0b5febacf102f6a7c4da486349bb3a00beb
todd <to0d@outlook.com>
2022-08-06 17:15:42 +0800

    up

--- test/beta/test/factor/XRFactorFixStmtTest.java
+++ test/beta/test/factor/XRFactorFixStmtTest.java
@@ -26,23 +26,9 @@ class XRFactorFixStmtTest extends RuleTestBase {
 	void test_fix_stmt_3_auto_remove_child_ref() {
 
 		_setup();
-		_test("(new model m)");
-		_test("(add-rule m if '(?a p1 ?b) '(?p p2 ?c) do (-> m '(?a p3 ?c)))");
-		_test("(add-stmt m '(a p1 b))");
-		_test("(add-stmt m '(c p2 d))");
-		_test("(start m)");
-		_test("(state-of m)", "completed");
-		_test("(list-stmt m)", "'('(a p1 b) '(c p2 d) '(a p3 d))");
+		_run_script();
 
-		_dumpEntryTable("m",
-				"result/factor/XRFactorFixStmtTest/test_fix_stmt_3_auto_remove_child_ref.entry.dump.1.txt");
-
-		_test("(fix-stmt m '(c p2 d))");
-		_dumpEntryTable("m",
-				"result/factor/XRFactorFixStmtTest/test_fix_stmt_3_auto_remove_child_ref.entry.dump.2.txt");
 	}
-	
-	
 
 	@Test
 	void test_fix_stmt_4_child_is_fix() {
--- test/beta/test/rule/TestReason.java
+++ test/beta/test/rule/TestReason.java
@@ -7,26 +7,15 @@ import alpha.rulp.utils.RuleTestBase;
 public class TestReason extends RuleTestBase {
 
 	@Test
-	public void test_3_circle_reason_a() {
+	void test_3_circle_reason_a() {
+
 		_setup();
-		// _enableTrace();
-		_test("(new model m)");
-		_test("(add-rule m if '(?x p ?y) '(p p2 p3) do (-> m '(?y p ?x)))");
-		_test("(add-stmt m '(a p c))");
-		_test("(add-stmt m '(p p2 p3))");
-		_test("(start m)", "9");
-		_test("(list-stmt m)", "'('(a p c) '(p p2 p3) '(c p a))");
-		_dumpEntryTable("m", "result/rule/TestReason/test_3_circle_reason_a.dump.1.txt");
-
-		_test("(remove-stmt m '(p p2 p3))");
-		_test("(list-stmt m)", "'('(a p c))");
-		_dumpEntryTable("m", "result/rule/TestReason/test_3_circle_reason_a.dump.2.txt");
-
+		_run_script();
 		_statsInfo("m");
 	}
 
 	@Test
-	public void test_3_circle_reason_b_named() {
+	void test_3_circle_reason_b_named() {
 		_setup();
 		_run_script();
 		_statsInfo("m");
