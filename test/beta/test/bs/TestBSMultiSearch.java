209060b2cd395109368427d493c72ff21730aff0
todd-t4 <lylybj@cn.ibm.com>
2022-09-13 23:31:18 +0800

    up

--- test/beta/test/bs/TestBSMultiSearch.java
+++ test/beta/test/bs/TestBSMultiSearch.java
@@ -37,7 +37,17 @@ public class TestBSMultiSearch extends RuleTestBase {
 	}
 
 	@Test
-	void test_bsm_1_int_var_2_a_query() {
+	void test_bsm_1_int_var_1_d_limit() {
+
+		_setup();
+		_run_script();
+		_statsInfo("m");
+		_dumpEntryTable("m");
+
+	}
+
+	@Test
+	void test_bsm_1_int_var_1_e_order_by_asc() {
 
 		_setup();
 		_run_script();
@@ -47,7 +57,37 @@ public class TestBSMultiSearch extends RuleTestBase {
 	}
 	
 	@Test
+	void test_bsm_1_int_var_1_f_order_by_desc() {
+
+		_setup();
+		_run_script();
+		_statsInfo("m");
+		_dumpEntryTable("m");
+
+	}
+
+	@Test
+	void test_bsm_1_int_var_2_a_query() {
+
+		_setup();
+		_run_script();
+		_statsInfo("m");
+		_dumpEntryTable("m");
+
+	}
+
+	@Test
 	void test_bsm_1_int_var_2_b_bs() {
+
+		_setup();
+		_run_script();
+		_statsInfo("m");
+		_dumpEntryTable("m");
+
+	}
+
+	@Test
+	void test_bsm_1_int_var_2_c() {
 
 		_setup();
 		_run_script();
