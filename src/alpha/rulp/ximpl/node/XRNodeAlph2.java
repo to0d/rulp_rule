7571de46b8333374279fb19d4b5b44d9200792df
todd-t4 <lylybj@cn.ibm.com>
2022-09-28 22:38:47 +0800

    up

---
+++ src/alpha/rulp/ximpl/node/XRNodeAlph2.java
@@ -0,0 +1,13 @@
+package alpha.rulp.ximpl.node;
+
+import alpha.rulp.lang.IRList;
+
+public class XRNodeAlph2 extends XRNodeRete1 {
+
+	protected IRList expectStmt;
+
+	public XRNodeAlph2(String instanceName) {
+		super(instanceName);
+	}
+
+}
