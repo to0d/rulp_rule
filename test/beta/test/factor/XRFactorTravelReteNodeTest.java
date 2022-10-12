5938ae6eff87f13275cb20e3e3394456877f3c14
todd-t4 <lylybj@cn.ibm.com>
2022-10-12 20:51:47 +0800

    up

---
+++ test/beta/test/factor/XRFactorTravelReteNodeTest.java
@@ -0,0 +1,16 @@
+package beta.test.factor;
+
+import org.junit.jupiter.api.Test;
+
+import alpha.rulp.utils.RuleTestBase;
+
+class XRFactorTravelReteNodeTest extends RuleTestBase {
+
+	@Test
+	void test_travel_rete_node_1_p2d() {
+		
+		_setup();
+		_run_script();
+	}
+
+}
